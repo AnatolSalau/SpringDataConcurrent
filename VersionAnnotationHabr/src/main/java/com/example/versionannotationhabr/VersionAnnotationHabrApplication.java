8669267764c1d71306c3aa4e97365f629ee0d830
AnatolSalau <salovanatoly5@mail.ru>
2024-03-21 20:25:03 +0300

    Initial commit

---
+++ VersionAnnotationHabr/src/main/java/com/example/versionannotationhabr/VersionAnnotationHabrApplication.java
@@ -0,0 +1,13 @@
+package com.example.versionannotationhabr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionAnnotationHabrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionAnnotationHabrApplication.class, args);
+	}
+
+}
---
+++ VersionAnnotationHabr/src/test/java/com/example/versionannotationhabr/VersionAnnotationHabrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.versionannotationhabr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionAnnotationHabrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
